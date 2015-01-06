68f7ed28e205f27f42e3bfbd2ba2cd114db6c059
Maruthi Prithivi <maruthiprithivi@gmail.com>
2015-01-06 23:34:59 +0800

    Updated
    added lemmatisation and stemming (06012015)

--- caps_nltk_101.py
+++ caps_nltk_101.py
@@ -3,14 +3,17 @@ __author__ = 'maruthi'
 import csv
 import nltk
 from nltk.tokenize import word_tokenize
-from nltk.tokenize import RegexpTokenizer
-from nltk.corpus import stopwords
-from nltk.corpus import wordnet
-# from nltk.tokenize import TreebankWordTokenizer
+from nltk.tokenize import RegexpTokenizer #Regular Expression Based Tokenizer
+from nltk.corpus import stopwords #Stopwords List
+from nltk.corpus import wordnet #WordNet
+from nltk.stem import WordNetLemmatizer #Lemmatizer
+from nltk.stem.porter import PorterStemmer #Stemmer
 
-# tokenizer = TreebankWordTokenizer()
+
 regToken = RegexpTokenizer("[\w']+")
 en_sw = set(stopwords.words('english'))
+wd_lm = WordNetLemmatizer()
+pr_sm = PorterStemmer()
 dataOpen = open("dataset/Data_101.csv", 'r')
 d1 = csv.reader(dataOpen,delimiter=',',quotechar='"')
 # print dataOpen
@@ -31,27 +34,21 @@ for i in d1:
     if y >= 3: break
     # wordToken = word_tokenize(i[14]) ## Testing
     wordToken = regToken.tokenize(i[14]) ## Actual
-    wordLow = [wordL.lower() for wordL in wordToken]
-    wordRemoved = [wordT for wordT in wordLow if wordT not in en_sw]
+    wordLow = [wordL.lower() for wordL in wordToken] ##Change to lower case
+    wordUp = [wordL.upper() for wordL in wordToken] ##Change to upper case
+    wordLem = [wd_lm.lemmatize(wordLe) for wordLe in wordLow] ##Run Through Lemmatization
+    wordStem = [pr_sm.stem(wordLe) for wordLe in wordLow] ##Run Through Stemming
+    wordRemoved = [wordT for wordT in wordLem if wordT not in en_sw] ##Removing StopWords
     # wordRemoved = [wordT for wordT in i[14] if wordT not in en_sw]
-    wordPos = nltk.pos_tag(wordRemoved)
+    wordPos = nltk.pos_tag(wordRemoved) ##POS Tagging
     if y > 1:
         print wordLow
+        print wordStem
+        print wordLem
         print wordRemoved
-        print wordPos
-        print type(wordPos)
+        # print wordPos
+        # print type(wordPos)
 
-    # print tokenizer(i[14])
-    # print type(i)
-    # dataRead = i.split(',')
-    # cnt = len(i)
-    # print dataRead
-    # print range(len(dataRead))
-    # print type(dataRead)
-    # for i,e in dataRead:
-    #     print e
-    #     x = x + 1
-    #     if x > 5: break
 
 
 print "Going Good"
