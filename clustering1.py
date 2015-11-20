ded1cfa087c323888022e358507165295ca53cb2
maruthiprithivi <maruthiprithivi@gmail.com>
2015-11-21 02:18:09 +0800

    Update clustering1.py
    still needs work

--- clustering1.py
+++ clustering1.py
@@ -1,5 +1,5 @@
 __author__ = 'maruthi'
-
+ 
 # Loading dependencies
 import sys
 import csv
@@ -22,9 +22,8 @@ import matplotlib as mpl
 from sklearn.manifold import MDS
 from nltk.tag import pos_tag
 from scipy.cluster.hierarchy import ward, dendrogram
-
-
-
+import io
+ 
 MDS()
 totalvocabStemmed1 = []
 totalvocabTokenized1 = []
@@ -56,40 +55,46 @@ r2 = 0
 r3 = 0
 r4 = 0
 r5 = 0
-
+ 
 reload(sys)
 sys.setdefaultencoding('utf8')
 en_sw = set(stopwords.words('english'))
-
+ 
 # Output
 # workbook = xlS.Workbook("markTwoPmiOne-Italian.xlsx")
 # worksheet0 = workbook.add_worksheet('PMI Score')
 # worksheet0.write(row0, col0, "Term")
 # worksheet0.write(row0, col0 + 1, "Positive Food PMI Score")
-
+ 
 # Tokenizing expression
 regToken = RegexpTokenizer("[\w']+")
-
+ 
 # For LDA - No use for now
 def strip_proppers_POS(text):
     tagged = pos_tag(text.split()) #use NLTK's part of speech tagger
     non_propernouns = [word for word,pos in tagged if pos != 'NNP' and pos != 'NNPS']
     return non_propernouns
-
+ 
 # Function for stemming
 def stemWord(word):
     stemmer = PorterStemmer()
     stemmedWord = stemmer.stem(word)
     return stemmedWord
-
+ 
 # Function for preparing bag of words
 def textPrepStem(review):
     preped = list()
     preped[:] = []
     # For getting rid of the stupid issue (Forgot the exact error) This will save you a lot of trouble
-    # review = unicode(review, errors='ignore')
+ 
     # Sklearn doesn't seem to support unicode
-    review = review.encode('ascii','ignore')
+    # try:
+    #     # review = unicode(review, errors='ignore')
+    #     review = review.encode('ascii','ignore')
+    #     # review = review.encode('utf-8')
+    # except:
+    #     print review
+    # review = review.encode('ascii','ignore')
     review = regToken.tokenize(review)
     review = [wordL.lower() for wordL in review]
 #     review = review.lower().split()
@@ -102,14 +107,20 @@ def textPrepStem(review):
         if word != "":
             preped.append(word)
     return preped
-
+ 
 def textPrep(review):
     preped = list()
     preped[:] = []
     # For getting rid of the stupid issue (Forgot the exact error) This will save you a lot of trouble
-    # review = unicode(review, errors='ignore')
+ 
     # Sklearn doesn't seem to support unicode
-    review = review.encode('ascii','ignore')
+    # try:
+    #     # review = unicode(review, errors='ignore')
+    #     review = review.encode('ascii','ignore')
+    #     # review = review.encode('utf-8')
+    # except:
+    #     print review
+    # review = review.encode('ascii','ignore')
     review = regToken.tokenize(review)
     review = [wordL.lower() for wordL in review]
     # Stop word removal, stemming(Porter Stemming) and basic text processing
@@ -120,12 +131,13 @@ def textPrep(review):
         if word != "":
             preped.append(word)
     return preped
-
+ 
 # Loading data
-dataOpen1 = codecs.open("Dataset/Data_101.csv", 'rU')
+# dataOpen1 = codecs.open("Dataset/Data_101.csv", 'rU')
+dataOpen1 = io.open("Dataset/aspectDataMerged.csv", encoding="ISO-8859-1", errors="ignore")
 d1 = csv.reader(dataOpen1,delimiter=',',quotechar='"', dialect=csv.excel_tab)
 d1.next()
-
+ 
 for i in d1:
     # Slicing the dataset based on the star ratings
     if i[13] == "1":
@@ -173,100 +185,107 @@ for i in d1:
         allwordsTokenized5 = textPrep(i[14])
         totalvocabTokenized5.extend(allwordsTokenized5)
         r5 += 1
-
-# print ("1 star rating count %d " % r1)
-# print ("2 star rating count %d " % r2)
-# print ("3 star rating count %d " % r3)
-# print ("4 star rating count %d " % r4)
-# print ("5 star rating count %d " % r5)
-
-print totalvocabTokenized1
-print totalvocabStemmed1
-
+ 
+print ("1 star rating count %d " % r1)
+print ("2 star rating count %d " % r2)
+print ("3 star rating count %d " % r3)
+print ("4 star rating count %d " % r4)
+print ("5 star rating count %d " % r5)
+ 
+ 
+ 
 vocabFrame1 = pd.DataFrame({'words': totalvocabTokenized1}, index = totalvocabStemmed1)
 vocabFrame2 = pd.DataFrame({'words': totalvocabTokenized2}, index = totalvocabStemmed2)
 vocabFrame3 = pd.DataFrame({'words': totalvocabTokenized3}, index = totalvocabStemmed3)
 vocabFrame4 = pd.DataFrame({'words': totalvocabTokenized4}, index = totalvocabStemmed4)
 vocabFrame5 = pd.DataFrame({'words': totalvocabTokenized5}, index = totalvocabStemmed5)
-
+ 
 tfidfVectorizer = TfidfVectorizer(max_df=0.8, max_features=20000000,
                                  min_df=0.4, stop_words='english',
-                                 use_idf=True, tokenizer=textPrepStem, ngram_range=(1,2))
-
+                                 use_idf=True, tokenizer=textPrepStem, ngram_range=(1,3))
+ 
 tfidfMatrix1 = tfidfVectorizer.fit_transform(bagOfReviews1)
 tfidfMatrix2 = tfidfVectorizer.fit_transform(bagOfReviews2)
 tfidfMatrix3 = tfidfVectorizer.fit_transform(bagOfReviews3)
 tfidfMatrix4 = tfidfVectorizer.fit_transform(bagOfReviews4)
 tfidfMatrix5 = tfidfVectorizer.fit_transform(bagOfReviews5)
-
+ 
 # print(tfidfMatrix1.shape)
 # print(tfidfMatrix2.shape)
 # print(tfidfMatrix3.shape)
 # print(tfidfMatrix4.shape)
 # print(tfidfMatrix5.shape)
-
+ 
 terms = tfidfVectorizer.get_feature_names()
-
+ 
 dist1 = 1 - cosine_similarity(tfidfMatrix1)
 dist2 = 1 - cosine_similarity(tfidfMatrix2)
 dist3 = 1 - cosine_similarity(tfidfMatrix3)
 dist4 = 1 - cosine_similarity(tfidfMatrix4)
 dist5 = 1 - cosine_similarity(tfidfMatrix5)
-
+ 
 numClusters = 5
-
-km = KMeans(n_clusters=numClusters)
-
-km1 = km.fit(tfidfMatrix1)
-km2 = km.fit(tfidfMatrix2)
-km3 = km.fit(tfidfMatrix3)
-km4 = km.fit(tfidfMatrix4)
-km5 = km.fit(tfidfMatrix5)
-
+ 
+km1 = KMeans(n_clusters=numClusters)
+km2 = KMeans(n_clusters=numClusters)
+km3 = KMeans(n_clusters=numClusters)
+km4 = KMeans(n_clusters=numClusters)
+km5 = KMeans(n_clusters=numClusters)
+ 
+km1.fit(tfidfMatrix1)
+km2.fit(tfidfMatrix2)
+km3.fit(tfidfMatrix3)
+km4.fit(tfidfMatrix4)
+km5.fit(tfidfMatrix5)
+ 
 clustersOne = km1.labels_.tolist()
 clustersTwo = km2.labels_.tolist()
 clustersThree = km3.labels_.tolist()
 clustersFour = km4.labels_.tolist()
 clustersFive = km5.labels_.tolist()
-
-#joblib.dump(km,  'doc_cluster.pkl')
+ 
+joblib.dump(km1,  'doc_cluster.pkl')
 km1 = joblib.load('doc_cluster.pkl')
+joblib.dump(km2,  'doc_cluster.pkl')
 km2 = joblib.load('doc_cluster.pkl')
+joblib.dump(km3,  'doc_cluster.pkl')
 km3 = joblib.load('doc_cluster.pkl')
+joblib.dump(km4,  'doc_cluster.pkl')
 km4 = joblib.load('doc_cluster.pkl')
+joblib.dump(km5,  'doc_cluster.pkl')
 km5 = joblib.load('doc_cluster.pkl')
-
+ 
 clustersOne = km1.labels_.tolist()
 clustersTwo = km2.labels_.tolist()
 clustersThree = km3.labels_.tolist()
 clustersFour = km4.labels_.tolist()
 clustersFive = km5.labels_.tolist()
-
-print clustersOne
-print clustersTwo
-print clustersThree
-print clustersFour
-print clustersFive
-
-
+ 
+# print clustersOne
+# print clustersTwo
+# print clustersThree
+# print clustersFour
+# print clustersFive
+ 
+ 
 oneStarReviews = { 'businessId': businessId1, 'reviews': bagOfReviews1, 'cluster': clustersOne, 'location': state1}
 twoStarReviews = { 'businessId': businessId2, 'reviews': bagOfReviews1, 'cluster': clustersTwo, 'location': state2}
 threeStarReviews = { 'businessId': businessId3, 'reviews': bagOfReviews1, 'cluster': clustersThree, 'location': state3}
 fourStarReviews = { 'businessId': businessId4, 'reviews': bagOfReviews1, 'cluster': clustersFour, 'location': state4}
 fiveStarReviews = { 'businessId': businessId5, 'reviews': bagOfReviews1, 'cluster': clustersFive, 'location': state5}
-
+ 
 reviewFrame1 = pd.DataFrame(oneStarReviews, index = [clustersOne] , columns = ['businessId', 'cluster', 'state'])
-reviewFrame2 = pd.DataFrame(oneStarReviews, index = [clustersTwo] , columns = ['businessId', 'cluster', 'state'])
-reviewFrame3 = pd.DataFrame(oneStarReviews, index = [clustersThree] , columns = ['businessId', 'cluster', 'state'])
-reviewFrame4 = pd.DataFrame(oneStarReviews, index = [clustersFour] , columns = ['businessId', 'cluster', 'state'])
-reviewFrame5 = pd.DataFrame(oneStarReviews, index = [clustersFive] , columns = ['businessId', 'cluster', 'state'])
-
+reviewFrame2 = pd.DataFrame(twoStarReviews, index = [clustersTwo] , columns = ['businessId', 'cluster', 'state'])
+reviewFrame3 = pd.DataFrame(threeStarReviews, index = [clustersThree] , columns = ['businessId', 'cluster', 'state'])
+reviewFrame4 = pd.DataFrame(fourStarReviews, index = [clustersFour] , columns = ['businessId', 'cluster', 'state'])
+reviewFrame5 = pd.DataFrame(fiveStarReviews, index = [clustersFive] , columns = ['businessId', 'cluster', 'state'])
+ 
 reviewFrame1['cluster'].value_counts()
 reviewFrame2['cluster'].value_counts()
 reviewFrame3['cluster'].value_counts()
 reviewFrame4['cluster'].value_counts()
 reviewFrame5['cluster'].value_counts()
-
+ 
 # Not required for now... since we don't have any kind of ranks for the reviews
 # groupOne = reviewFrame1['businessId'].groupby(reviewFrame1['cluster'])
 # groupTwo = reviewFrame2['businessId'].groupby(reviewFrame2['cluster'])
@@ -279,113 +298,121 @@ reviewFrame5['cluster'].value_counts()
 # groupThree.mean()
 # groupFour.mean()
 # groupFive.mean()
-
-
+ 
+ 
 # Cluster 1
 print("Top terms per clusterOne:")
 print()
 orderCentroids = km1.cluster_centers_.argsort()[:, ::-1]
 for i in range(numClusters):
     print("Cluster %d words:" % i)
-    for ind in orderCentroids[i, :6]:
+    print len(orderCentroids)
+    for ind in orderCentroids[i, :]:
+        print ind
         print(' %s' % vocabFrame1.ix[terms[ind].split(' ')].values.tolist()[0][0].encode('utf-8', 'ignore'))
     print()
     print()
     print("Cluster %d BusinessId:" % i)
-    for feature in vocabFrame1.ix[i]['businessId'].values.tolist():
+    for feature in reviewFrame1.ix[i]['businessId'].values.tolist():
         print(' %s, ' % feature)
     print()
     print()
-
+ 
 # Cluster 2
 print("Top terms per clusterOne:")
 print()
 orderCentroids = km2.cluster_centers_.argsort()[:, ::-1]
 for i in range(numClusters):
     print("Cluster %d words:" % i)
-    for ind in orderCentroids[i, :6]:
+    print len(orderCentroids)
+    for ind in orderCentroids[i, :]:
         print(' %s' % vocabFrame2.ix[terms[ind].split(' ')].values.tolist()[0][0].encode('utf-8', 'ignore'))
     print()
     print()
     print("Cluster %d BusinessId:" % i)
-    for feature in vocabFrame2.ix[i]['businessId'].values.tolist():
+    for feature in reviewFrame2.ix[i]['businessId'].values.tolist():
         print(' %s, ' % feature)
     print()
     print()
-
+ 
 # Cluster 3
 print("Top terms per clusterOne:")
 print()
 orderCentroids = km3.cluster_centers_.argsort()[:, ::-1]
 for i in range(numClusters):
     print("Cluster %d words:" % i)
-    for ind in orderCentroids[i, :6]:
+    print len(orderCentroids)
+    for ind in orderCentroids[i, :]:
         print(' %s' % vocabFrame3.ix[terms[ind].split(' ')].values.tolist()[0][0].encode('utf-8', 'ignore'))
     print()
     print()
     print("Cluster %d BusinessId:" % i)
-    for feature in vocabFrame3.ix[i]['businessId'].values.tolist():
+    for feature in reviewFrame3.ix[i]['businessId'].values.tolist():
         print(' %s, ' % feature)
     print()
     print()
-
+ 
 # Cluster 4
 print("Top terms per clusterOne:")
 print()
 orderCentroids = km4.cluster_centers_.argsort()[:, ::-1]
 for i in range(numClusters):
     print("Cluster %d words:" % i)
-    for ind in orderCentroids[i, :6]:
+    print len(orderCentroids)
+    for ind in orderCentroids[i, :]:
         print(' %s' % vocabFrame4.ix[terms[ind].split(' ')].values.tolist()[0][0].encode('utf-8', 'ignore'))
     print()
     print()
     print("Cluster %d BusinessId:" % i)
-    for feature in vocabFrame4.ix[i]['businessId'].values.tolist():
+    for feature in reviewFrame4.ix[i]['businessId'].values.tolist():
         print(' %s, ' % feature)
     print()
     print()
-
+ 
 # Cluster 5
 print("Top terms per clusterOne:")
 print()
 orderCentroids = km5.cluster_centers_.argsort()[:, ::-1]
 for i in range(numClusters):
     print("Cluster %d words:" % i)
-    for ind in orderCentroids[i, :6]:
+    print len(orderCentroids)
+    for ind in orderCentroids[i, :]:
         print(' %s' % vocabFrame5.ix[terms[ind].split(' ')].values.tolist()[0][0].encode('utf-8', 'ignore'))
     print()
     print()
     print("Cluster %d BusinessId:" % i)
-    for feature in vocabFrame5.ix[i]['businessId'].values.tolist():
+    for feature in reviewFrame5.ix[i]['businessId'].values.tolist():
         print(' %s, ' % feature)
     print()
     print()
-
+ 
 reviewFrame1['bizId'] = reviewFrame1['businessId']
 reviewFrame2['bizId'] = reviewFrame2['businessId']
 reviewFrame3['bizId'] = reviewFrame3['businessId']
 reviewFrame4['bizId'] = reviewFrame4['businessId']
 reviewFrame5['bizId'] = reviewFrame5['businessId']
-
+ 
 print(reviewFrame1[['bizId']].loc[reviewFrame1['cluster'] == 1].to_html(index=False))
 print(reviewFrame2[['bizId']].loc[reviewFrame2['cluster'] == 1].to_html(index=False))
 print(reviewFrame3[['bizId']].loc[reviewFrame3['cluster'] == 1].to_html(index=False))
 print(reviewFrame4[['bizId']].loc[reviewFrame4['cluster'] == 1].to_html(index=False))
 print(reviewFrame5[['bizId']].loc[reviewFrame5['cluster'] == 1].to_html(index=False))
-
-
+ 
+ 
 mds = MDS(n_components=2, dissimilarity="precomputed", random_state=1)
-
-pos1 = mds.fit_transform(dist1)
-pos2 = mds.fit_transform(dist2)  
-pos3 = mds.fit_transform(dist3)  
-pos4 = mds.fit_transform(dist4)  
-pos5 = mds.fit_transform(dist5)  
-
+ 
+pos1 = mds.fit_transform(dist1)  # shape (n_components, n_samples)
+pos2 = mds.fit_transform(dist2)  # shape (n_components, n_samples)
+pos3 = mds.fit_transform(dist3)  # shape (n_components, n_samples)
+pos4 = mds.fit_transform(dist4)  # shape (n_components, n_samples)
+pos5 = mds.fit_transform(dist5)  # shape (n_components, n_samples)
+ 
 xs1, ys1 = pos1[:, 0], pos1[:, 1]
 xs2, ys2 = pos2[:, 0], pos2[:, 1]
 xs3, ys3 = pos3[:, 0], pos3[:, 1]
 xs4, ys4 = pos4[:, 0], pos4[:, 1]
 xs5, ys5 = pos5[:, 0], pos5[:, 1]
-
+ 
 # "In [49]" Visualizing document clusters
+ 
+print "Hakuna MaTaTaAaAaaaaA!!"
